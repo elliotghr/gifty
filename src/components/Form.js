b1dcb1961fe133a2baaa48a3a7f8e0f2d8b4b272
ElliotGhr <gusnroses.2617@gmail.com>
2023-03-22 20:06:31 -0600

    Implementando useReducer en Form

--- src/components/Form.js
+++ src/components/Form.js
@@ -1,24 +1,55 @@
-import React, { useState } from "react";
+import React, { useReducer } from "react";
 import { useNavigate } from "react-router-dom";
 
 const RATINGS = ["g", "pg", "pg-13", "r"];
+const ACTIONS = {
+  UPDATE_KEYWORD: "UPDATE_KEYWORD",
+  UPDATE_RATING: "UPDATE_RATING",
+};
+
+const reducer = (state, action) => {
+  switch (action.type) {
+    case ACTIONS.UPDATE_KEYWORD:
+      return {
+        ...state,
+        keyword: action.payload,
+        times: state.times + 1,
+      };
+    case ACTIONS.UPDATE_RATING:
+      return {
+        ...state,
+        rating: action.payload,
+      };
+    default:
+      return {
+        state,
+      };
+  }
+};
 
 const Form = ({ initialKeyword = "", initialRating }) => {
-  const [keyword, setKeyword] = useState(initialKeyword);
-  const [rating, setRating] = useState(initialRating);
+  // const [rating, setRating] = useState(initialRating);
   const navigate = useNavigate();
 
+  const [state, dispatch] = useReducer(reducer, {
+    keyword: initialKeyword,
+    times: 0,
+    rating: initialRating,
+  });
+
+  const { keyword, times, rating } = state;
+
   const handleChange = (e) => {
-    setKeyword(e.target.value);
+    dispatch({ type: ACTIONS.UPDATE_KEYWORD, payload: e.target.value });
   };
   const handleSubmit = (e) => {
     e.preventDefault();
     navigate(`/search/${keyword}/${rating}`);
   };
-
   const handleChangeRating = (e) => {
-    setRating(e.target.value);
+    dispatch({ type: ACTIONS.UPDATE_RATING, payload: e.target.value });
   };
+
   return (
     <form onSubmit={handleSubmit}>
       <label>Buscar</label>
@@ -33,6 +64,7 @@ const Form = ({ initialKeyword = "", initialRating }) => {
           <option key={el}>{el}</option>
         ))}
       </select>
+      <small style={{ color: "whitesmoke" }}>{times}</small>
     </form>
   );
 };
---
+++ src/notes/notes.md
@@ -0,0 +1,4 @@
+# Notas
+
+- Agregar un "?" en el path de una ruta que recibe parametros nos permite volverlo opcional, esto nos puede dar una retrocompatibilidad y que se pueda seguir mostrando la ruta sin necesidad de que estén todos los parametros presentes
+ 
